5c363b45a11e8ba30f1f47ad7a70a7be32db1cdc
jiacheng-x <jxu728@outlook.com>
2024-03-23 02:08:38 -0400

    update

---
+++ R/edge_detect.R
@@ -0,0 +1,217 @@
+#' Function to detect edges/jumps on SQUARE image, i.e. equal width and height
+#'
+#' @param z Input image
+#' @param k Tuning parameter, size of neighborhood for edge calculation
+#' @param n Image width/height,
+#' @param beta1 Least Square estimate from LS_estimate function
+#' @param beta2 Least Square estimate from LS_estimate function
+#' @param alpha_n Parameter to adjust detection sensitivity, default 0.05
+#'
+#' @return An 0, 1 matrix of edge locations with 1 being edge location
+#' @export
+#'
+#' @examples
+EdgeDectect <- function(z, k, n, beta1, beta2, alpha_n = 0.05) {
+  k1 <- (k + 1) / 2
+  neigh <- matrix(rep(0, 4), nrow = 2)
+  delta <- matrix(rep(0, n * n), nrow = n)
+  threshold <- matrix(rep(0, n * n), nrow = n)
+  edge <- matrix(rep(0, n * n), nrow = n)
+  for (i in (k + k1):(n - k - k1 + 1)) {
+    for (j in (k + k1):(n - k - k1 + 1)) {
+      deno <- beta1[i, j]^2 + beta2[i, j]^2
+      if (deno == 0) {
+        cat("No edge detected at (", i, ", ", j, ").\n")
+        break
+      } else {
+        cond <- abs(beta2[i, j]) / sqrt(deno)
+        if (cond > sqrt(2) / 2) {
+          neigh[1, 1] <- trunc(i + k / beta2[i, j] * beta1[i, j])
+          neigh[1, 2] <- j + k
+          neigh[2, 1] <- trunc(i - k / beta2[i, j] * beta1[i, j])
+          neigh[2, 2] <- j - k
+        } else {
+          neigh[1, 1] <- i + k
+          neigh[1, 2] <- trunc(j + k * beta2[i, j] / beta1[i, j])
+          neigh[2, 1] <- i - k
+          neigh[2, 2] <- trunc(j - k * beta2[i, j] / beta1[i, j])
+        }
+        temp1 <- 0
+        temp2 <- 0
+        temp3 <- 0
+        z_temp1 <- c()
+        z_temp2 <- c()
+        z_temp3 <- c()
+        for (i1 in (max(neigh[1, 1] - k1 + 1, 1)):(min(neigh[1, 1] + k1 - 1, n))) {
+          for (j1 in (max(neigh[1, 2] - k1 + 1, 1)):(min(neigh[1, 2] + k1 - 1, n))) {
+            temp1 <- temp1 + z[i1, j1]
+            z_temp1 <- c(z_temp1, z[i1, j1])
+          }
+        }
+        for (i1 in (max(neigh[2, 1] - k1 + 1, 1)):(min(neigh[2, 1] + k1 - 1, n))) {
+          for (j1 in (max(neigh[2, 2] - k1 + 1, 1)):(min(neigh[2, 2] + k1 - 1, n))) {
+            temp2 <- temp2 + z[i1, j1]
+            z_temp2 <- c(z_temp2, z[i1, j1])
+          }
+        }
+        for (i1 in (i - k1 + 1):(i + k1 - 1)) {
+          for (j1 in (j - k1 + 1):(j + k1 - 1)) {
+            temp3 <- temp3 + z[i1, j1]
+            z_temp3 <- c(z_temp3, z[i1, j1])
+          }
+        }
+        delta[i, j] <- min(abs(temp1 - temp3), abs(temp2 - temp3)) / (k * k)
+        var_hat <- (var(z_temp3) + var(z_temp1)) * (abs(temp1 - temp3) > abs(temp2 - temp3)) +
+          (var(z_temp3) + var(z_temp2)) * (1 - (abs(temp1 - temp3) > abs(temp2 - temp3))) # use sample variance to estimate variance in threshold calculation
+        threshold[i, j] <- qnorm(1 - alpha_n / 2) * sqrt(2 * var_hat) / k
+      }
+    }
+  }
+
+  for (i in (k + k1):(n - k - k1 + 1)) {
+    for (j in (k + k1):(n - k - k1 + 1)) {
+      if (delta[i, j] > threshold[i, j]) {
+        edge[i, j] <- 1
+      }
+    }
+  }
+
+  ## modify four corners
+  # up-left
+  sum <- 0
+  for (i in (k + k1):(2 * k + k1 - 1)) {
+    for (j in (k + k1):(2 * k + k1 - 1)) {
+      sum <- sum + edge[i, j]
+    }
+  }
+  if (sum <= k1) {
+    for (i in (k + k1):(2 * k)) {
+      for (j in (k + k1):(2 * k)) {
+        edge[i, j] <- 0
+      }
+    }
+  }
+  # up-right
+  sum <- 0
+  for (i in (n - 2 * k - k1 + 2):(n - k - k1 + 1)) {
+    for (j in (k + k1):(2 * k + k1 - 1)) {
+      sum <- sum + edge[i, j]
+    }
+  }
+  if (sum <= k1) {
+    for (i in (n - 2 * k + 1):(n - k - k1 + 1)) {
+      for (j in (k + k1):(2 * k)) {
+        edge[i, j] <- 0
+      }
+    }
+  }
+  # down-left
+  sum <- 0
+  for (i in (k + k1):(2 * k + k1 - 1)) {
+    for (j in (n - 2 * k - k1 + 2):(n - k - k1 + 1)) {
+      sum <- sum + edge[i, j]
+    }
+  }
+  if (sum <= k1) {
+    for (i in (k + k1):(2 * k)) {
+      for (j in (n - 2 * k + 1):(n - k - k1 + 1)) {
+        edge[i, j] <- 0
+      }
+    }
+  }
+  # down-right
+  sum <- 0
+  for (i in (n - 2 * k - k1 + 2):(n - k - k1 + 1)) {
+    for (j in (n - 2 * k - k1 + 2):(n - k - k1 + 1)) {
+      sum <- sum + edge[i, j]
+    }
+  }
+  if (sum <= k1) {
+    for (i in (n - 2 * k + 1):(n - k - k1 + 1)) {
+      for (j in (n - 2 * k + 1):(n - k - k1 + 1)) {
+        edge[i, j] <- 0
+      }
+    }
+  }
+  # upper boundary
+  for (i in (2 * k + 1):(n - 2 * k)) {
+    for (j in (k + k1):(2 * k)) {
+      sum <- 0
+      if (edge[i, j] == 1) {
+        for (i1 in (i - k1 + 1):(i + k1 - 1)) {
+          for (j1 in (k + k1):(2 * k + k1 - 1)) {
+            sum <- sum + edge[i1, j1]
+          }
+        }
+        if (sum <= k1) {
+          edge[i, j] <- 0
+        }
+      }
+    }
+  }
+  # down boundary
+  for (i in (2 * k + 1):(n - 2 * k)) {
+    for (j in (n - 2 * k + 1):(n - k - k1 + 1)) {
+      sum <- 0
+      if (edge[i, j] == 1) {
+        for (i1 in (i - k1 + 1):(i + k1 - 1)) {
+          for (j1 in (n - 2 * k - k1 + 2):(n - k - k1 + 1)) {
+            sum <- sum + edge[i1, j1]
+          }
+        }
+        if (sum <= k1) {
+          edge[i, j] <- 0
+        }
+      }
+    }
+  }
+  # left boundary
+  for (j in (2 * k + 1):(n - 2 * k)) {
+    for (i in (k + k1):(2 * k)) {
+      sum <- 0
+      if (edge[i, j] == 1) {
+        for (j1 in (j - k1 + 1):(j + k1 - 1)) {
+          for (i1 in (k + k1):(2 * k + k1 - 1)) {
+            sum <- sum + edge[i1, j1]
+          }
+        }
+        if (sum <= k1) {
+          edge[i, j] <- 0
+        }
+      }
+    }
+  }
+  # right boundary
+  for (j in (2 * k + 1):(n - 2 * k)) {
+    for (i in (n - 2 * k + 1):(n - k - k1 + 1)) {
+      sum <- 0
+      if (edge[i, j] == 1) {
+        for (j1 in (j - k1 + 1):(j + k1 - 1)) {
+          for (i1 in (n - 2 * k - k1 + 2):(n - k - k1 + 1)) {
+            sum <- sum + edge[i1, j1]
+          }
+        }
+        if (sum <= k1) {
+          edge[i, j] <- 0
+        }
+      }
+    }
+  }
+  # center boundary
+  for (i in (2 * k + 1):(n - 2 * k)) {
+    for (j in (2 * k + 1):(n - 2 * k)) {
+      sum <- 0
+      if (edge[i, j] == 1) {
+        for (i1 in (i - k1 + 1):(i + k1 - 1)) {
+          for (j1 in (j - k1 + 1):(j + k1 - 1)) {
+            sum <- sum + edge[i1, j1]
+          }
+        }
+        if (sum <= k1) {
+          edge[i, j] <- 0
+        }
+      }
+    }
+  }
+  return(edge)
+}
